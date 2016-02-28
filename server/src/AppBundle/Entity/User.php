af2f79e332a622f4d83b4060fe477ebeabba9c44
kamedon <hkamei06@gmail.com>
2016-02-29 08:41:08 +0900

    install fosuserbundle

---
+++ server/src/AppBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kamedon
+ * Date: 2/25/16
+ * Time: 10:54 PM
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
