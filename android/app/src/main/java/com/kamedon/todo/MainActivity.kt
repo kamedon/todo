673328a2de6847eb398b2650df15ff39573182c6
kamedon <hkamei06@gmail.com>
2016-02-29 14:51:50 +0900

    ログイン画面のレイアウトを作成

---
+++ android/app/src/main/java/com/kamedon/todo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kamedon.todo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by kamedon on 2/29/16.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+    }
+}
