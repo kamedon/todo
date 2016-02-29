8c7ca5c026ba0f39646b9cfb06563aa1499803a7
kamedon <hkamei06@gmail.com>
2016-02-29 11:28:25 +0900

    task apiのurlを作った

---
+++ server/src/AppBundle/Controller/Api/TaskApiController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kamedon
+ * Date: 2/29/16
+ * Time: 11:24 AM
+ */
+
+namespace AppBundle\Controller\Api;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\UuidValidator;
+
+class TaskApiController extends RestController
+{
+
+    /**
+     * @ApiDoc(
+     *     description="タスク登録",
+     *     statusCodes={
+     *         200="created task",
+     *         403="Header:X-User-Agent-Authorizationの認証失敗 または　apiKeyの認証失敗",
+     *     }
+     * )
+     * @param Request $request
+     * @return array
+     */
+    public function postTasksAction(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * @ApiDoc(
+     *     description="タスク一覧",
+     *     statusCodes={
+     *         200="list task",
+     *         403="Header:X-User-Agent-Authorizationの認証失敗 または　apiKeyの認証失敗",
+     *     }
+     * )
+     * @param Request $request
+     * @return array
+     */
+    public function getTasksAction(Request $request)
+    {
+        return [];
+    }
+}
--- server/src/AppBundle/Controller/Api/UserApiController.php
+++ server/src/AppBundle/Controller/Api/UserApiController.php
@@ -13,20 +13,10 @@ use AppBundle\Entity\ApiKey;
 use AppBundle\Entity\NewUserRequest;
 use AppBundle\Form\NewUserFormType;
 use AppBundle\Form\UserType;
-use FOS\RestBundle\Controller\FOSRestController;
-use FOS\UserBundle\Event\FormEvent;
-use FOS\UserBundle\Event\GetResponseUserEvent;
-use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\Config\Definition\Exception\Exception;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\Uuid;
-use Symfony\Component\Validator\Constraints\UuidValidator;
 
 class UserApiController extends RestController
 {
