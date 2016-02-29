31562a036ba05f69aa0c2becc2527db16bb6e0be
kamedon <hkamei06@gmail.com>
2016-02-29 16:14:55 +0900

    タスクページのレイアウトのひな形を作成

--- android/app/src/main/java/com/kamedon/todo/TaskActivity.kt
+++ android/app/src/main/java/com/kamedon/todo/TaskActivity.kt
@@ -1,9 +1,17 @@
 package com.kamedon.todo
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 
 /**
  * Created by kamedon on 2/29/16.
  */
 class TaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
 }
